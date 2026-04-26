8e33b28b58e8ecb2389f6c043d4472c2e46ad56f
Akshatk-me <kushwahakshat32@gmail.com>
2026-04-27 03:44:21 +0530

    Added utils.py with convenient func for testing

---
+++ sim/utils.py
@@ -0,0 +1,16 @@
+from cocotb.triggers import RisingEdge
+
+
+async def reset(dut):
+    dut.reset.value = 1
+    await RisingEdge(dut.clk)
+    dut.reset.value = 0
+
+
+async def step(dut, n=1):
+    for _ in range(n):
+        await RisingEdge(dut.clk)
+
+
+def pc(dut):
+    return dut.pc_out.value.integer
